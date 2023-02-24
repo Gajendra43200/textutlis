31d0f87dedff537b9d2d93a46ca9fb07a9e5b3d2
Prince4320 <gajendrapatel173@gmail.com>
2023-02-24 14:02:14 +0530

    Updates

---
+++ precache-manifest.c0de2bc6404f52a4896589cfda4ecf6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0fe85057da660057ef7",
+    "url": "/textutlis/static/css/main.3cbc1e73.chunk.css"
+  },
+  {
+    "revision": "d0fe85057da660057ef7",
+    "url": "/textutlis/static/js/main.d0fe8505.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/textutlis/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "4b3c2d27ce7566aa7f06",
+    "url": "/textutlis/static/js/2.4b3c2d27.chunk.js"
+  },
+  {
+    "revision": "2460f0a76829043d906d",
+    "url": "/textutlis/static/js/runtime~main.2460f0a7.js"
+  },
+  {
+    "revision": "cd118610cd9e111f578f39deb8bb1026",
+    "url": "/textutlis/index.html"
+  }
+];
